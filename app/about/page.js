8ffb773bc381dcc3939961e0ee48291385b3b9a1
GouthamGopu <gopugoutham3@gmail.com>
2025-02-07 22:05:03 +0530

    .

--- app/about/page.js
+++ app/about/page.js
@@ -11,19 +11,19 @@ const About = () => {
               About Us
             </div>
             <div className="text-center font-bold text-white">
-              "Fueling Creativity, One Cup at a Time"
+            &quot;Fueling Creativity, One Cup at a Time&quot;
             </div>
             <p className="p-4 text-gray-200 text-md text-center font-sans">
               At Get Me A Chai, we are committed to fostering a vibrant community where developers, creators, and influencers can thrive. Our crowdfunding platform is designed to connect talented individuals with supporters who believe in their vision and want to help bring their projects to life. We understand that financial support is crucial for creativity and innovation, and we strive to make the process seamless and rewarding for both creators and backers.
             </p>
             <p className="p-4 text-gray-200 text-md text-center font-sans">
-              Our mission is to empower developers, content creators, artists, and influencers by providing them with the resources they need to succeed. Whether you're a developer working on the next groundbreaking app, a content creator producing engaging and informative videos, an artist creating stunning visual works, or an influencer sharing your unique perspective with the world, Get Me A Chai is here to support you every step of the way.
+              Our mission is to empower developers, content creators, artists, and influencers by providing them with the resources they need to succeed. Whether you&apos;re a developer working on the next groundbreaking app, a content creator producing engaging and informative videos, an artist creating stunning visual works, or an influencer sharing your unique perspective with the world, Get Me A Chai is here to support you every step of the way.
             </p>
             <p className="p-4 text-gray-200 text-md text-center font-sans">
               We believe in the transformative power of community support and the magic that happens when people come together to champion a common cause. Our platform is more than just a crowdfunding site; it's a space where dreams can become reality, and innovative ideas can flourish. By providing a straightforward and user-friendly way for patrons to contribute, we aim to create a supportive ecosystem that nurtures creativity and drives progress.
             </p>
             <p className="p-4 text-gray-200 text-md text-center font-sans">
-              At Get Me A Chai, we take pride in helping creators reach their full potential. We are passionate about leveraging the power of technology to build a community that values and invests in creative endeavors. Join us on this exciting journey and be part of a movement that celebrates and sustains the talents of developers, creators, and influencers. Together, let's fuel creativity, one cup at a time.
+              At Get Me A Chai, we take pride in helping creators reach their full potential. We are passionate about leveraging the power of technology to build a community that values and invests in creative endeavors. Join us on this exciting journey and be part of a movement that celebrates and sustains the talents of developers, creators, and influencers. Together, let&apos;s fuel creativity, one cup at a time.
             </p>
           </div>
           {/* <div className="absolute top-0 left-0 z-0 h-1/3 w-full" style={{ backgroundImage: 'linear-gradient(to left bottom, rgba(79, 70, 229, 0.2) 0%, transparent 50%, transparent 100%)', borderColor: 'rgba(92, 79, 240, 0.2)' }}>
@@ -51,7 +51,7 @@ const About = () => {
                 </svg>
               </div>
               <h3 className="mt-6 text-gray-400">Easy Customization</h3>
-              <p className="my-4 mb-0 font-normal leading-relaxed tracking-wide text-gray-400">Personalize your campaign page's look and feel, from color schemes to fonts, to match your unique style.</p>
+              <p className="my-4 mb-0 font-normal leading-relaxed tracking-wide text-gray-400">Personalize your campaign page&apos;s look and feel, from color schemes to fonts, to match your unique style.</p>
             </div>
             <div className="rounded-md border border-neutral-800 bg-neutral-900/50 p-8 text-center shadow">
               <div className="button-text mx-auto flex h-12 w-12 items-center justify-center rounded-md border" style={{ backgroundImage: 'linear-gradient(rgb(80, 70, 229) 0%, rgb(43, 49, 203) 100%)', borderColor: 'rgb(93, 79, 240)' }}>
